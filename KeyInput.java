82ebdf5a1a896912617675d6a64b2433371ea7a9
Shahen M <smatevosyan246@gmail.com>
2017-06-22 18:54:34 -0700

    KeyInput.java

---
+++ KeyInput.java
@@ -0,0 +1,21 @@
+package com.main.bricks;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+
+	private Game game;
+	
+	public KeyInput(Game game) {
+		this.game = game;
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		game.keyPressed(e);
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		game.keyReleased(e);
+	}	
+}
